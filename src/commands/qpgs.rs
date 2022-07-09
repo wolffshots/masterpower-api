014648bfe40dd7316681f314e579ec0d460b1887
jadon <16850875+wolffshots@users.noreply.github.com>
2022-07-09 17:11:25 +0200

    feat!: added qpgs2 for secondary

--- src/commands/qpgs.rs
+++ src/commands/qpgs.rs
@@ -5,21 +5,30 @@ use serde_derive::Serialize;
 use std::str::from_utf8;
 use log::{debug};
 
+pub struct QPGS0;
+pub struct QPGS2;
 pub struct QPGS1;
 
-impl Command for QPGS1 {
-    const PROTOCOL_ID: &'static [u8] = b"QPGS1";
-    const COMMAND_NAME: &'static str = "QueryDeviceGeneralStatusFirstAdditional";
+impl Command for QPGS0 {
+    const PROTOCOL_ID: &'static [u8] = b"QPGS0";
+    const COMMAND_NAME: &'static str = "QueryDeviceGeneralStatusCurrent";
 
     type Request = ();
     type Response = QGPSResponse;
 }
 
-pub struct QPGS0;
-
-impl Command for QPGS0 {
-    const PROTOCOL_ID: &'static [u8] = b"QPGS0";
+impl Command for QPGS1 {
+    const PROTOCOL_ID: &'static [u8] = b"QPGS1";
     const COMMAND_NAME: &'static str = "QueryDeviceGeneralStatusPrimary";
+
+    type Request = ();
+    type Response = QGPSResponse;
+}
+
+
+impl Command for QPGS2 {
+    const PROTOCOL_ID: &'static [u8] = b"QPGS2";
+    const COMMAND_NAME: &'static str = "QueryDeviceGeneralStatusSecondary";
 
     type Request = ();
     type Response = QGPSResponse;
